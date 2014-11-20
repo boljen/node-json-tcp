bb92a3ebd28917d58c321c1290bdbbfaa47d6b96
Christophe Bol <bol.christophe@gmail.com>
2014-11-20 13:35:03 +0100

    some tweaks

--- test/var/protocol.js
+++ test/var/protocol.js
@@ -5,11 +5,31 @@ var framework = require('javascript-protocol-framework')
 var proto = new framework.Protocol();
 
 
+proto.addReceiver('test', function() {
+  console.log('gotten test');
+})
+
+var crypto = require('crypto');
+var createCipher= function(key, iv) {
+  return crypto.createCipheriv('aes-192-cfb8', key, iv);
+};
+
+var createDecipher= function(key, iv) {
+  return crypto.createDecipheriv('aes-192-cfb8', key, iv);
+};
+
+
 net.createServer(function(s) {
   var ses = new framework.Session({
     protocol: proto
   });
-  protocol(ses, s, s);
+  var enc = createCipher('testtesttesttesttesttest', 'testtesttesttest');
+  var dec = createDecipher('testtesttesttesttesttest', 'testtesttesttest');
+
+  s.pipe(dec);
+  enc.pipe(s);
+
+  protocol(ses, dec, enc);
 
   ses.send({
     type: 'test',
@@ -22,7 +42,11 @@ var c = net.connect(80);
 var ses = new framework.Session({
   protocol: proto
 });
-ses.on('test', function(msg, sess) {
-  console.log(msg.data)
-})
-protocol(ses, c);
+
+var enc = createCipher('testtesttesttesttesttest', 'testtesttesttest');
+var dec = createDecipher('testtesttesttesttesttest', 'testtesttesttest');
+
+c.pipe(dec);
+enc.pipe(c);
+
+protocol(ses, dec, enc);
